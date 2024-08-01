ada9211a28d3c9d74b82c90c2330757eaa77b989
honglegedou <1926058528@qq.com>
2024-08-01 16:57:57 +0800

    Site updated: 2024-08-01 16:57:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/11/MY SQL/","2024/07/13/数仓概念/"];function toRandomPost(){
+var posts=["2024/07/13/数仓概念/","2024/07/11/MY SQL/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
